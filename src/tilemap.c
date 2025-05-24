76a5e0bc20c5b4c15440d4bca788feaa8a3fa424
orta-afk <darkkboy0013@gmail.com>
2025-05-25 02:52:35 +0530

    omg this shouldn't take this long time

--- src/tilemap.c
+++ src/tilemap.c
@@ -1,4 +1,5 @@
 #include "tilemap.h"
+#include <raylib.h>
 
 int map[WIDTH][HEIGHT];
 
@@ -10,14 +11,29 @@ void initTilemap(tilemap *tilemap){
 }
 
 void updateTilemap(tilemap *tilemap){
-  for(int i = 0; i < WIDTH; i++){
-    for(int j = 0; j < HEIGHT; j++){
-      if(i >= 0 || i <= 40){
-        map[i][j] = tile;
-      }      
+  int screenTilesX = WIDTH;  
+  int screenTilesY = HEIGHT;
+
+  int left   = (screenTilesX / 2);
+  int right  = (screenTilesX / 2);
+  int top    = (screenTilesY / 2);
+  int bottom = (screenTilesY / 2);
+
+  for (int i = 0; i < WIDTH; i++) {
+    for (int j = 0; j < HEIGHT; j++) {
+      if (i >= 3 && i <= 37 && j >= 3 && j <= 17) {
+        if(i >= left && i <= right && j >= top && j <= bottom){
+          map[i][j] = backgroundTile;
+        }else{
+          map[i][j] = tile;
+        }
+      } else {
+        map[i][j] = backgroundTile;  
+      }
     }
   }
 }
+
 
 void drawTilemap(tilemap *tilemap) {
   for (int i = 0; i < WIDTH; i++) {
