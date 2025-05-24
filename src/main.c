47c559a9e6609b1b27ad5e948d3be8104e2e886e
orta-afk <darkkboy0013@gmail.com>
2025-05-24 08:43:36 +0530

    look a entity

--- src/main.c
+++ src/main.c
@@ -1,23 +1,31 @@
 #include "raylib.h"
+#include "entity.h"
 #include "shopRoom.h"
 
 int main(){
   InitWindow(640, 320, "omega");
 
+  entity entity;
+  entityTexture et;
   shopRoom shoproom;
 
+  initEntity(&entity);
+  initEntityTexture(&et);
   initShop(&shoproom);
 
   while(!WindowShouldClose()){
+    updateEntity(&entity, &et);
     updateShop(&shoproom);
 
     BeginDrawing();
     ClearBackground(BLACK);
     drawShop(&shoproom);
+    drawEntity(&et);
     EndDrawing();
   }
 
   destroyShop(&shoproom);
+  destroyEntity(&et);
   CloseWindow();
   return 0;
 }
